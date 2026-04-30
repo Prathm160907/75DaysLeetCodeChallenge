d80e1d70a8433953fa6d17d9626f1210d88f14a9
Prathm160907 <prathameshteke96@gmail.com>
2026-04-30 17:02:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3908-valid-digit-number/3908-valid-digit-number.java
+++ 3908-valid-digit-number/3908-valid-digit-number.java
@@ -1,14 +1,20 @@
 class Solution {
     public boolean validDigit(int n, int x) {
-        String s = Integer.toString(n);
-        char ch = (char) (x + '0');
         int c=0;
-        for(int i =0;i<s.length();i++){
-            if(s.charAt(i)==ch){
+        int fc = 0;
+        int nc =0;
+        while(n!=0){
+            int l = n%10;
+            if(l==x){
                 c++;
             }
+            nc = n;
+            n /=10;
+            if(n==0){
+                fc = nc;
+            }
         }
-        if(s.charAt(0)!=ch && c>0){
+        if(fc != x && c>0){
             return true;
         }
         return false;
