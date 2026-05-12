279a3621df4eb4a17757a8b08197ce6c4a87fea5
Prathm160907 <prathameshteke96@gmail.com>
2026-05-12 22:38:31 +0530

    Time: 1 ms (100%), Space: 47.1 MB (12.3%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -2,9 +2,6 @@ class Solution {
     public int[] countOppositeParity(int[] nums) {
         int en =0;
         int od = 0;
-        if(nums.length==1){
-            return new int[]{0};
-        }
         for(int i =0;i<nums.length;i++){
             if(nums[i]%2==0){
                 en++;
